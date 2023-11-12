028f0bef7e84d751761fdc1e4d4d8d3aff591868
Eric Kariri <51946943+karirieric@users.noreply.github.com>
2023-11-13 02:10:43 +0300

    Add files via upload

---
+++ src/carts/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+
+from orders.models import Order
+
+from products.models import Products
+
+from .models import Cart
+
+def cart_home(request):
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, "carts/home.html", {"cart":cart_obj})
+
+def cart_update(request):
+    print(request.POST)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Products.objects.get(id=product_id)
+        except Products.DoesNotExist:
+           print("Product is gone?? 🥲")
+           return redirect("cart:home")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
+        request.session['cart_items'] = cart_obj.products.count()
+        # return redirect(product_obj.get_absolute_url())
+    return redirect("cart:home")
+
+def checkout_home(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect("cart:home")
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    return render(request, "carts/checkout.html", {"object": order_obj})
+
